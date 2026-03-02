d2794cd62cc410112d1edba85bd0d3638ade22c0
qué valentin? <95167780+qvalentln@users.noreply.github.com>
2026-03-02 15:20:03 +0200

    Create main.cpp with file I/O and setup
    Initialize main.cpp with input/output file handling and basic setup.

---
+++ main.cpp
@@ -0,0 +1,30 @@
+/*
+    author: qvalentin
+    
+    dumnezeu sa o ierte...
+    
+    chr(69) + chr(88) + chr(88)
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define ll long long
+#define pb push_back
+#define MOD 1000000007
+#define fastio ios_base::sync_with_stdio(0); cin.tie(nullptr);
+int di[4]={0,0,-1,1};
+int dj[4]={-1,1,0,0};
+
+string FILENAME="cresc";
+ifstream f(FILENAME+".in");
+ofstream g(FILENAME+".out");
+
+
+int main(){
+    fastio    
+    #define cin f
+    #define cout g
+    
+
+    return 0;
+}
