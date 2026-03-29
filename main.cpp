5d9b6adf46b1e7028fd510c312dd87875a9f9c9b
qué valentin? <95167780+qvalentln@users.noreply.github.com>
2026-03-30 01:20:22 +0300

    mi-e scarba de ala vechi, dar si asta nou e la fel

--- main.cpp
+++ main.cpp
@@ -1,30 +1,31 @@
-/*
-    author: qvalentin
+/*  
+    * author: qvalentin  
+
     
-    dumnezeu sa o ierte...
+    * chr(69) + chr(88) + chr(88)  
+    * https://codeforces.com/profile/qvalentin
     
-    chr(69) + chr(88) + chr(88)
+    * it's you, it'll always be you *
+    
 */
-#include <bits/stdc++.h>
-using namespace std;
-#define ull unsigned long long
-#define ll long long
-#define pb push_back
-#define MOD 1000000007
-#define fastio ios_base::sync_with_stdio(0); cin.tie(nullptr);
-int di[4]={0,0,-1,1};
-int dj[4]={-1,1,0,0};
 
-string FILENAME="cresc";
-ifstream f(FILENAME+".in");
-ofstream g(FILENAME+".out");
+#include <bits/stdc++.h>  
+using namespace std;  
+#define ull unsigned long long  
+#define ll long long  
+#define pb push_back  
+#define fastio ios_base::sync_with_stdio(0); cin.tie(nullptr);  
+const int MOD = 1e9+7;
+int di[4]={0,0,-1,1};  
+int dj[4]={-1,1,0,0};  
+      
+string FILENAME="alee";  
+ifstream f(FILENAME+".in");  
+ofstream g(FILENAME+".out");  
 
-
-int main(){
-    fastio    
-    #define cin f
-    #define cout g
-    
+signed main(){  
 
     return 0;
+    
 }
+
